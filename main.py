57c748ceab53b04e76f63de91c4f3f54a3b9a6dc
“codebydhae” <dhaesha3000@gmail.com>
2024-12-30 09:33:57 -0500

    simple contact input

---
+++ main.py
@@ -0,0 +1,9 @@
+#get user input
+def getContact():
+    name = input("Enter name: ")
+    number = input("Enter number: ")
+    print(f" contact: {name}, {number}")
+    return name, number
+getContact()
+#contact list container
+#delete contact   
