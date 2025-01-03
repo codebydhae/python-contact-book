9945446538bb60214d1207314e95186e7a19ad4a
“codebydhae” <dhaesha3000@gmail.com>
2025-01-03 18:44:42 -0500

    basic contact logic created and ready for testing

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+#contact list:
+contactList = []
+
 #get user input
-def getContact():
+def add_contact():
     name = input("Enter name: ")
     number = input("Enter number: ")
-    print(f" contact: {name}, {number}")
-    return name, number
-getContact()
-#contact list container
-#delete contact   
+    contact = name, number
+    #print(contact)
+    contactList.append(contact)
+    
+
+def show_contact(contactList):
+    print(contactList)
+
+def delete_contact(contactList):
+    choice = input("Enter the name of contact you would like to delete.")
+    #selected
+    for contact in contactList:
+        if choice == contact.name:
+            contactList.remove(contact)
+    print(f"You removed ${contact} from list.")
+ 
